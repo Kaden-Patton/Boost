51d7244a69df963e19cb01a6c120d1b11025767c
Unknown <KadenPatton@protonmail.com>
2018-07-05 08:41:40 -0600

    Audio!
    Added rocket thrust sound effect when user holds down space.
    Also added functionality for turning rocket left/right.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
+
 
 	// Use this for initialization
 	void Start () {
-        rigidBody = GetComponent<Rigidbody>();	
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -22,17 +25,25 @@ public class Rocket : MonoBehaviour {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            print("A was pressed.");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            print("D was pressed.");
+            transform.Rotate(-Vector3.forward);
         }
 
     }
 
-}
+} // End of class
